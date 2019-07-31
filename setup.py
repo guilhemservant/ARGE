009ef86bc96e8bcaabc6fb36c283f74f362540c8
Guilhem Servant <guilhem.servant.etu@univ-lille.fr>
2019-07-31 13:55:17 +0100

    typo

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[ 'pandas>=0.21.0', 'numpy'],
+    install_requires=['pandas>=0.21.0', 'numpy'],
     entry_points={'console_scripts': ['ARGE=ARGE.ARGE:entry_point']}
 )
